3617568b650eb5acf080fc0618feffc22c9c1df1
x-finity <x.finity777@gmail.com>
2024-04-23 15:57:15 +0300

    vkapi: changed sex var in get_user_info

--- modules/vkapi.py
+++ modules/vkapi.py
@@ -1,6 +1,5 @@
 import vk_api
 
-
 class VkUser:
     def __init__(self, token):
         self.token = token
@@ -14,7 +13,7 @@ class VkUser:
                 user_info = user_info[0]
                 first_name = user_info['first_name']
                 last_name = user_info['last_name']
-                sex = user_info['sex']
+                sex = True if user_info['sex'] == 1 else False if user_info['sex'] == 2 else None
                 city = user_info.get('city', {}).get('title', 'Не указано')
                 return {'first_name': first_name, 'last_name': last_name, 'sex': sex, 'city': city}
             else:
@@ -25,7 +24,9 @@ class VkUser:
 
 
 if __name__ == "__main__":
-    token = ...
+    import os
+    from database import load_config
+    token = load_config(os.path.dirname(__file__) + '/../config.json')['vk_token']
     user_id = input("Введите введите ID пользователя: ")
     vk_user = VkUser(token)
     user_info = vk_user.get_user_info(user_id)
