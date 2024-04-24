ceabe7646e3dec888c37127d3cf74711926b27a8
x-finity <x.finity777@gmail.com>
2024-04-24 03:16:35 +0300

    refactor VkGroupAPI/VkUserAPI classes add get_user_photos, get_user_id func

--- modules/vkapi.py
+++ modules/vkapi.py
@@ -5,10 +5,8 @@ from datetime import date
 def get_age(bdate):
     if bdate.count('.') == 2:
         bdate = date(*[int(x) for x in bdate.split('.')[::-1]])
-    elif bdate.count('.') == 1:
-        bdate = date(*[int(x) for x in bdate.split('.')[::-1]],1)
     else:
-        bdate = date(int(bdate), 1, 1)
+        return 0
     today = date.today()
     age = today.year - bdate.year
     if today.month < bdate.month or (today.month == bdate.month and today.day < bdate.day):
@@ -16,7 +14,15 @@ def get_age(bdate):
     return age
 
 
-class VkUser:
+class VkGroupAPI:
+    def __init__(self, token):
+        self.token = token
+        self.vk_session = vk_api.VkApi(token=token)
+        self.vk = self.vk_session.get_api()
+
+
+class VkUserAPI:
+
     def __init__(self, token):
         self.token = token
         self.vk_session = vk_api.VkApi(token=token)
@@ -44,18 +50,38 @@ class VkUser:
             print("Ошибка при получении информации о пользователе:", e)
             return None
 
+    def get_user_photos(self, user_id):
+        photos = self.vk.photos.get(owner_id=user_id, album_id='profile', extended=1).get('items')
+        return photos
+
+    def get_user_id(self, user_name):
+        if isinstance(user_name, int):
+            return user_name
+        elif user_name.isdigit():
+            return int(user_name)
+        user = self.vk.users.get(user_ids=user_name)
+        return user[0].get('id')
+    def get_search_result(self, city, age, sex, age_delta=5):
+        return self.vk.users.search(count=100, hometown=city, age_from=age - age_delta, age_to=age + age_delta, sex=sex)
+
 
 if __name__ == "__main__":
     from database import load_config
-    token = load_config()['VK_TOKEN']
-    print("Токен VK:", token)
-    user_id = input("Введите введите ID пользователя: ")
-    vk_user = VkUser(token)
-    user_info = vk_user.get_user_info(user_id)
-    if user_info:
-        print("Имя:", user_info['first_name'])
-        print("Фамилия:", user_info['last_name'])
-        print("Пол:", "Женский" if user_info['sex'] == 1 else "Мужской")
-        print("Город:", user_info['city'])
-    else:
-        print("Не удалось получить информацию о пользователе.")
+    token = load_config()['VK_GROUP_TOKEN']
+    # print("Токен VK:", token)
+    user_id = input("Введите введите ID пользователя: ") or 126875243
+    # vk_group = VkGroupAPI(token)
+    # user_info = vk_group.get_user_info(user_id)
+    # if user_info:
+    #     print("Имя:", user_info['first_name'])
+    #     print("Фамилия:", user_info['last_name'])
+    #     print("Пол:", "Женский" if user_info['sex'] == 1 else "Мужской")
+    #     print("Город:", user_info['city'])
+    #     print("Возраст:", user_info['age'])
+    # else:
+    #     print("Не удалось получить информацию о пользователе.")
+
+    vk_user = VkUserAPI(load_config()['VK_USER_TOKEN'])
+    # print(vk_user.get_user_info(user_id))
+    # print(vk_user.get_search_result('Москва', 30, 1))
+    print(vk_user.get_user_photos(user_id))
