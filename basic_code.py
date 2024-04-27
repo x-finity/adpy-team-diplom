bd07ba020341c7c1013d07cd057960a630a60cb8
Danila Shashko <danila12308@gmail.com>
2024-04-27 13:55:33 +0300

    bot

--- basic_code.py
+++ basic_code.py
@@ -1,53 +1,68 @@
 import vk_api
-import json
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+import json
 from modules.database import load_config, create_session
 config = load_config()
 session = create_session(config)
 
 
-class VkBot:
-    def __init__(self, token):
-        self.token = token
-        self.vk_session = vk_api.VkApi(token=token)
-        self.vk = self.vk_session.get_api()
-        self.longpoll = VkLongPoll(self.vk_session)
-
-        self.keyboard = self.generate_keyboard([['Привет', 'Пока']], one_time=False)
-
-    def generate_keyboard(self, buttons, one_time=True):
-        keyboard = {
-            "one_time": one_time,
-            "buttons": [[self.get_button(text, 'positive') for text in row] for row in buttons]
-        }
-        return json.dumps(keyboard, ensure_ascii=False).encode('utf-8').decode('utf-8')
-
-    def get_button(self, text, color):
-        return {
-            "action": {
-                "type": "text",
-                "payload": "{\"button\": \"" + "1" + "\"}",
-                "label": text
-            },
-            "color": color
-        }
-
-    def sender(self, user_id, message):
-        self.vk_session.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': 0, 'keyboard': self.keyboard})
-
-    def handle_message(self, event):
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.to_me:
-                user_id = event.user_id
-                message = event.text.lower()
-                self.sender(user_id, message.upper())
-
-    def start(self):
-        for event in self.longpoll.listen():
-            self.handle_message(event)
+class User:
+    def __init__(self, id, mode, cash):
+        self.id = id
+        self.mode = mode
+        self.cash = cash
 
 
-if __name__ == "__main__":
-    token = ...
-    bot = VkBot(token)
-    bot.start()
+def Keyboard(buttons):
+    new_buttons = []
+    for i in range(len(buttons)):
+        new_buttons.append([])
+        for j in range(len(buttons[i])):
+            new_buttons[i].append(None)
+    for i in range(len(buttons)):
+        for j in range(len(buttons[i])):
+            text = buttons[i][j][0]
+            color = {'Зеленый': 'positive', 'Красный': 'negative', 'Серый': 'secondary'}[buttons[i][j][1]]
+            new_buttons[i][j] = {'action': {'type': 'text', 'payload': '{"button": "1"}', 'label': text},
+                                 'color': color}
+    return new_buttons
+
+
+start_key = Keyboard([
+    [('Начать', 'Зеленый')],
+])
+
+next_key = Keyboard([
+    [('Назад', 'Красный'), ('Поиск людей', 'Зеленый')],
+])
+
+search_key = Keyboard([
+    [('Назад', 'Красный'), ('Заполнить форму', 'Зеленый')],
+])
+
+parameters_key = Keyboard([
+    [('Назад', 'Красный'), ('Пол', 'Зеленый'), ('Возраст', 'Зеленый')],
+])
+
+
+def sender(user_id, message, keyboard):
+    vk_session.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': 0, 'keyboard': json.dumps({'buttons': keyboard})})
+
+
+users = {}
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            if event.text.lower() == "начать":
+                sender(event.user_id, "Привет!", next_key)
+            elif event.text.lower() == "поиск людей":
+                sender(event.user_id, "Выберите действие:", search_key)
+            elif event.text.lower() == "заполнить форму":
+                sender(event.user_id, "Заполните форму!", parameters_key)
+                if event.text.lower() == "назад":
+                    sender(event.user_id, "Выберите действие:", search_key)
+            elif event.text.lower() == "назад":
+                sender(event.user_id, "Выберите действие:", start_key)
